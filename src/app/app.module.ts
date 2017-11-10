0fcbb39d3c89bffdc40600ee8b09ef9345928b4a
Loic Martinez <martinez_65@hotmail.fr>
2017-11-10 11:48:50 +0100

    Liste des posts + détails post + Liste des users + détails user

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {PostsModule} from './posts/posts.module';
+import {RouterModule, Routes} from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: '',
+    redirectTo: '/posts',
+    pathMatch: 'full'
+  }]
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PostsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/posts/post.ts
@@ -0,0 +1,7 @@
+export class Post {
+  id: number;
+  content: string;
+  img?: string;
+  timestamp?: number;
+}
+
